eff5fbc5dd960870a5c7846c4b88fe059713792e
凌柏超 <1585638808@qq.com>
2021-06-25 07:02:56 +0800

    Update shim-tsx.d.ts

--- vue2/src/shim-tsx.d.ts
+++ vue2/src/shim-tsx.d.ts
@@ -1,9 +1,11 @@
 import Vue, { VNode } from 'vue'
+import { ComponentRenderProxy } from '@vue/composition-api';
 
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
     interface Element extends VNode {}
+    interface ElementClass extends ComponentRenderProxy {}
     // tslint:disable no-empty-interface
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
