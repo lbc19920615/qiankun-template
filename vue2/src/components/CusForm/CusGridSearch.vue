b6d85124ad4201a1c3c3de4b4be27cef1cc396e8
凌柏超 <1585638808@qq.com>
2021-06-25 06:52:43 +0800

    添加grid支持

---
+++ vue2/src/components/CusForm/CusGridSearch.vue
@@ -0,0 +1,23 @@
+<style lang="scss">
+$tag: "cus-grid-search";
+$sel: "." + $tag;
+
+#{$sel} {
+}
+</style>
+
+<template>
+  <div class="cus-grid-search">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CusGridSearch',
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ vue2/src/components/CusForm/CusGridWrap.vue
@@ -0,0 +1,23 @@
+<style lang="scss">
+$tag: "cus-grid-wrap";
+$sel: "." + $tag;
+
+#{$sel} {
+}
+</style>
+
+<template>
+  <div class="cus-grid-wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CusGridWrap',
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  data () {
+    return {}
+  }
+}
+</script>
