40628d6dbd531143cbb1028871bc7e7b5bf8104a
凌柏超 <1585638808@qq.com>
2021-06-25 20:32:39 +0800

    add new number input

--- vue2/src/plugins/cusform.js
+++ vue2/src/plugins/cusform.js
@@ -2,3 +2,7 @@ import Vue from 'vue'
 import IntergerInput from '@/components/FreeForm/IntergerInput'
 
 Vue.component('interger-input', IntergerInput)
+
+
+import VueNumberInput from '@chenfengyuan/vue-number-input';
+Vue.component(VueNumberInput.name, VueNumberInput);
